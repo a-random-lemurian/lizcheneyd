1c4e8313c8f5efcae60e2680ca19c3732af02ac2
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-27 01:03:26 +0800

    chore: update imgdown.h decls

--- core/imgdown.h
+++ core/imgdown.h
@@ -2,14 +2,14 @@
 #define IMGDOWN_H
 #include <openssl/sha.h>
 
-void lizcheneyd_get_image_of(const char* person);
-void lizcheneyd_sha256_string(char hash[SHA256_DIGEST_LENGTH], char outbuf[65]);
-int lizcheneyd_sha256_file(const char *filename, char outputBuffer[65]);
-void verify_liz_cheney_image(const char* filename);
-void get_liz_cheney_image();
-void lizcheneyd_set_uagent(char* new_agent);
 int should_extract_images();
 void set_should_extract_image(int new_value);
+void lizcheneyd_set_uagent(char *new_agent);
+void lizcheneyd_get_image_of(const char *person);
+void lizcheneyd_sha256_string(char hash[SHA256_DIGEST_LENGTH], char outbuf[65]);
+int lizcheneyd_sha256_file(const char *filename, char outputBuffer[65]);
+int lizcheneyd_verify_file(const char *filename, const char *sha256_checksum);
+void verify_liz_cheney_image(const char *filename);
+void lizcheneyd_get_image(const char *url, const char *out_path, int is_binary);
 int lizcheneyd_check_dir_access_wrapper();
-
 #endif /* IMGDOWN_H */
