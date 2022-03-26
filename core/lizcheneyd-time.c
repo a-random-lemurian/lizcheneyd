711661c7de0d33825ed90c052d18a15d9a8c20d1
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-26 19:15:48 +0800

    style: enforce style in lizcheneyd-time.c

--- core/lizcheneyd-time.c
+++ core/lizcheneyd-time.c
@@ -4,13 +4,13 @@
 
 int is_liz_cheney_birthday()
 {
-    time_t current_time = time(NULL);
-    struct tm time_struct = *localtime(&current_time);
+  time_t current_time = time(NULL);
+  struct tm time_struct = *localtime(&current_time);
 
-    if (time_struct.tm_mday == 28 && time_struct.tm_mon == 7) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+  if (time_struct.tm_mday == 28 && time_struct.tm_mon == 7) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
 }
