d154991f306fc6b384925471f866af79daf79600
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-26 19:42:38 +0800

    chore: update lizcheneyd.h functions

--- core/lizcheneyd.h
+++ core/lizcheneyd.h
@@ -2,9 +2,13 @@
 #define LIZCHENEYD_H
 #include <stddef.h>
 
-void lizcheneyd();
 int get_cycles_before_shutdown();
 size_t get_cycles_done();
 void set_cycles_before_shutdown(int new_value);
+void lizcheneyd_sigint_handler();
+void get_images_of_liz_cheney();
+void lizcheneyd_syslog_message(log_Event *le);
+void lizcheneyd_init_logging();
+void lizcheneyd();
 
 #endif /* LIZCHENEYD_H */
