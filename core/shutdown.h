538309f381276213b78f7af1c0ad0e1594aa3735
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-03-26 19:41:35 +0800

    fix: update shutdown.h include guard

--- core/shutdown.h
+++ core/shutdown.h
@@ -1,5 +1,5 @@
-#ifndef SHUTDOWN_CONDITIONS_H
-#define SHUTDOWN_CONDITIONS_H
+#ifndef SHUTDOWN_H
+#define SHUTDOWN_H
 
 #define CYCLE_TARGET_REACHED 1
 #define SIGINT_CAUGHT 2
@@ -8,4 +8,4 @@ void lizcheneyd_should_shutdown();
 void lizcheneyd_shutdown_because_of(int reason);
 void lizcheneyd_shutdown(int exit_code);
 
-#endif /* SHUTDOWN_CONDITIONS_H */
+#endif /* SHUTDOWN_H */
